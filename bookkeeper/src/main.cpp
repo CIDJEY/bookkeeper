6ccf6519ee9e0612f9634d53a4d834f6a995a798
Alexander Samoylov <a.samoylov@activevideo.com>
2022-11-21 17:01:15 +0100

    Asio hello world

--- bookkeeper/src/main.cpp
+++ bookkeeper/src/main.cpp
@@ -1,8 +1,15 @@
 
 
 #include <iostream>
+#include <asio.hpp>
 
 int main(int argc, char** argv) {
+
+	asio::io_context io;
+
+	asio::steady_timer timer(io, asio::chrono::seconds(3));
+
+	timer.wait();
 	std::cout << "Hello world\n";
 	return 0;
 }
