f1bcc2f94ae889c3bc597e21ca6573d4be340be4
Alexander Samoylov <a.samoylov@activevideo.com>
2022-12-06 22:39:11 +0100

    spdlog

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -6,10 +6,10 @@
 int main(int argc, char** argv) {
 	try {
 		spdlog::set_level(spdlog::level::debug);
-		spdlog::info("Hello world! from the bookkeeper!");
+
+		spdlog::info("Hello world! from the client!");
 	} catch (const std::exception& error) {
 		std::cerr << error.what() << std::endl;
 	}
 
-	return 0;
 }
